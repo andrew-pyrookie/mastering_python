58fdaeac37f7d8d9d91b1eaf6745c5258fb7b853
andrew-pyrookie <andrewthepyrookie@gmail.com>
2024-03-01 05:44:52 +0300

    first commit

---
+++ classes_objects.py
@@ -0,0 +1,12 @@
+# python is and object oriented programming language
+# object has properties and methods
+
+'''
+Creating a class with name MyClass with property named x
+'''
+
+class MyClass:
+    x = 5
+print(MyClass)    
+    
+    
